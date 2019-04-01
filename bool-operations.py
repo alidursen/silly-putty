2674a14e93501353ccb833f430933209ac905436
Ali Kutlu Durşen <alidursen@gmail.com>
2019-04-01 13:35:19 +0300

    Update bool-operations.py

--- bool-operations.py
+++ bool-operations.py
@@ -1,6 +1,10 @@
 """
 A restatement of existing `and` and `or` operations.
 Note that returned items need not be `bool`-valued.
+
+Also note that, since OR(x,y) is different than OR(y,x),
+this also means `x or y` is different than `y or x`
+(and vice versa for `and`.)
 """
 
 def OR(x,y):
